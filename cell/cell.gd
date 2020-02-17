d1d12bdee4f5c3b1605b36951da1a24a87b8b257
Daniel Higuita <daniel.higuita007@gmail.com>
2020-02-16 19:33:05 -0500

    map

--- cell/cell.gd
+++ cell/cell.gd
@@ -3,6 +3,11 @@ extends Sprite
 class_name Cell
 
 var element
+var cell_size: Vector2
+var area2D
+
+func _ready():
+	self.area2D = $Area2D
 
 func set_element(new_element) -> void:
 	if not self.element:
@@ -13,8 +18,6 @@ func clear_element() -> void:
 
 func _on_Area2D_area_entered(area):
 	self.set_element(area)
-
-
 
 func _on_Area2D_body_entered(body):
 	self.set_element(body)
