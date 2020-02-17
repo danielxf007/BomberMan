2d90cbc98912f94e3be58b4729c7f834ee83e5a8
Daniel Higuita <daniel.higuita007@gmail.com>
2020-02-17 14:56:25 -0500

    cell_container

--- board/board.gd
+++ board/board.gd
@@ -4,6 +4,7 @@ class_name Board
 
 export(Vector2) var cell_dimensions = Vector2(1, 1)
 export(Vector2) var board_top_pos: Vector2 = Vector2()
+export(Vector2) var CELL_CONTAINER_SIZE: Vector2 = Vector2()
 export(Texture) var ON_CELL
 export(Texture) var OFF_CELL
 export(bool) var PATTERN_ON_OFF = true
@@ -18,12 +19,14 @@ func _ready():
 	self.board_size = Vector2(floor(self.window.size.x/self.cell_dimensions.x),
 	floor(self.window.size.y/ self.cell_dimensions.y))
 	self.matrix_of_cells = self.create_matrix(self.board_top_pos,
-	self.board_size, self.cell_dimensions, preload("res://cell/Cell.tscn"))
+	self.board_size, self.cell_dimensions, self.CELL_CONTAINER_SIZE,
+	preload("res://cell/Cell.tscn"))
 	self.organize_matrix_of_cells(self.PATTERN_ON_OFF, 
 	self.board_size, self.ON_CELL, self.OFF_CELL)
 
 func create_matrix(matrix_top_pos: Vector2, dimensions: Vector2,
- cell_size: Vector2, cell_scene: PackedScene) -> Array:
+ cell_size: Vector2, cell_container_size: Vector2,
+ cell_scene: PackedScene) -> Array:
 	var one_x_n_matrix: Array = []
 	var cell: Cell
 	var before_cell: Cell
@@ -32,6 +35,7 @@ func create_matrix(matrix_top_pos: Vector2, dimensions: Vector2,
 			cell = cell_scene.instance()
 			self.add_child(cell)
 			cell.cell_size = cell_size
+			cell.cell_container_size = cell_container_size
 			before_cell = one_x_n_matrix[i-1][0]
 			cell.position.x = matrix_top_pos.x
 			cell.position.y = (before_cell.position.y +
@@ -41,6 +45,7 @@ func create_matrix(matrix_top_pos: Vector2, dimensions: Vector2,
 			cell = cell_scene.instance()
 			self.add_child(cell)
 			cell.cell_size = cell_size
+			cell.cell_container_size = cell_container_size
 			cell.position = matrix_top_pos
 			one_x_n_matrix.append([cell])
 	var cell_matrix: Array = []
@@ -51,6 +56,7 @@ func create_matrix(matrix_top_pos: Vector2, dimensions: Vector2,
 			cell = cell_scene.instance()
 			self.add_child(cell)
 			cell.cell_size = cell_size
+			cell.cell_container_size = cell_container_size
 			before_cell = cell_matrix[i][j-1]
 			cell.position.x = (before_cell.position.x +
 			before_cell.cell_size.x)
--- cell/cell.gd
+++ cell/cell.gd
@@ -4,10 +4,7 @@ class_name Cell
 
 var element
 var cell_size: Vector2
-var area2D
-
-func _ready():
-	self.area2D = $Area2D
+var cell_container_size: Vector2
 
 func set_element(new_element) -> void:
 	if not self.element:
@@ -15,19 +12,3 @@ func set_element(new_element) -> void:
 
 func clear_element() -> void:
 	self.element = null
-
-func _on_Area2D_area_entered(area):
-	self.set_element(area)
-
-func _on_Area2D_body_entered(body):
-	self.set_element(body)
-
-
-# warning-ignore:unused_argument
-func _on_Area2D_area_exited(area):
-	self.clear_element()
-
-
-# warning-ignore:unused_argument
-func _on_Area2D_body_exited(body):
-	self.clear_element()
