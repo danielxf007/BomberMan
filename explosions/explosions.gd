55d6e4cfb823c81e14c210368df2bcc08791db81
Daniel Higuita <daniel.higuita007@gmail.com>
2020-02-19 17:54:45 -0500

    explosions

---
+++ explosions/explosions.gd
@@ -0,0 +1,80 @@
+extends Node
+
+class_name Explosions
+
+var util_f: UtilFunctions = UtilFunctions.new()
+func cell_content_can_be_destroyed(cell: Cell) -> bool:
+	return cell.element.has_method("destroy")
+
+func row_explosion(_range: Range, column_index: int,
+row_cells: Array) -> Array:
+	var cell: Cell
+	var row_explosion_range: Array = []
+	var indexes: Tuple
+	for j in _range:
+		cell = row_cells[j]
+		if not cell.is_occupied():
+			indexes = Tuple.new(j, column_index)
+			row_explosion_range.append(indexes)
+		elif self.cell_content_can_be_destroyed(cell):
+			indexes = Tuple.new(j, column_index)
+			row_explosion_range.append(indexes)
+		elif not self.cell_content_can_be_destroyed(cell):
+			return row_explosion_range
+	return row_explosion_range
+
+func column_explosion(_range: Range, row_index: int,
+columns_cells: Array) -> Array:
+	var cell: Cell
+	var column_explosion_range: Array = []
+	var indexes: Tuple
+	for i in _range:
+		cell = columns_cells[i]
+		if not cell.is_occupied():
+			indexes = Tuple.new(i, row_index)
+			column_explosion_range.append(indexes)
+		elif self.cell_content_can_be_destroyed(cell):
+			indexes = Tuple.new(i, row_index)
+			column_explosion_range.append(indexes)
+		elif not self.cell_content_can_be_destroyed(cell):
+			return column_explosion_range
+	return column_explosion_range
+
+func diagonal_explosions_types(explosion_type: int) -> Tuple:
+	var movement_type: Tuple = Tuple.new(null, null)
+	match explosion_type:
+		0: 
+			movement_type = Tuple.new(1, -1)
+			break
+		1:
+			movement_type = Tuple.new(-1, -1)
+			break
+		2:
+			movement_type = Tuple.new(-1, 1)
+			break
+		3:
+			movement_type = Tuple.new(1, 1)
+	return movement_type
+
+func diagonal_explosion(board_dimensions: Tuple, starting_point: Tuple,
+explosion_type: int, matrix_of_cells: Array) -> Array:
+	var indexes: Tuple
+	var movement: Tuple = self.diagonal_explosions_types(explosion_type)
+	var diagonal_explosion_range: Array = []
+	var i = starting_point.first_element + movement.first_element
+	var j = starting_point.second_element + movement.second_element
+	var cell: Cell
+	while (self.util_f.number_in_range(0, board_dimensions.first_element, i)
+	and self.util_f.number_in_range(0, board_dimensions.second_element, j)):
+		cell = matrix_of_cells[i][j]
+		if not cell.is_occupied():
+			indexes = Tuple.new(i, j)
+			diagonal_explosion_range.append(indexes)
+		elif self.cell_content_can_be_destroyed(cell):
+			indexes = Tuple.new(i, j)
+			diagonal_explosion_range.append(indexes)
+		elif not self.cell_content_can_be_destroyed(cell):
+			return diagonal_explosion_range
+		i+= movement.first_element
+		j+= movement.second_element
+	return diagonal_explosion_range
