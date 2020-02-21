d5ec0a2f4a7fc4b369474d7a7e19b99a8de96922
Daniel Higuita <daniel.higuita007@gmail.com>
2020-02-21 17:44:29 -0500

    bomb

--- bomb/bomb_explosions/cross_explosion.gd
+++ bomb/bomb_explosions/cross_explosion.gd
@@ -9,25 +9,22 @@ export(int) var DOWN_RANGE = 0
 
 
 func explode( explosion_point: Tuple, matrix_of_cells: Array) -> Array:
-	var left_explosion: Array = self.filter_explosions(
-		matrix_of_cells, self.get_row_explosion(
+	var center: Array = [explosion_point]
+	var left_explosion: Array = self.get_row_explosion(
 		self.get_left_range(self.LEFT_RANGE, explosion_point),
-		 explosion_point, matrix_of_cells)
+		 explosion_point, matrix_of_cells
 	)
-	var right_explosion: Array = self.filter_explosions(
-		matrix_of_cells, self.get_row_explosion(
+	var right_explosion: Array = self.get_row_explosion(
 		self.get_right_range(self.RIGHT_RANGE, explosion_point),
-		explosion_point, matrix_of_cells)
+		explosion_point, matrix_of_cells
 	)
-	var up_explosion: Array = self.filter_explosions(
-		matrix_of_cells, self.get_column_explosion(
+	var up_explosion: Array = self.get_column_explosion(
 		self.get_up_range(self.UP_RANGE, explosion_point),
-		explosion_point, matrix_of_cells)
+		explosion_point, matrix_of_cells
 	)
-	var down_explosion: Array = self.filter_explosions(
-		matrix_of_cells, self.get_column_explosion(
+	var down_explosion: Array = self.get_column_explosion(
 		self.get_down_range(self.DOWN_RANGE, explosion_point),
-		explosion_point, matrix_of_cells)
+		explosion_point, matrix_of_cells
 	)
-	return (left_explosion + right_explosion + 
+	return (center + left_explosion + right_explosion + 
 	up_explosion + down_explosion)
